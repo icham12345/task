607c04ff9fe26ff24c1d1c29fd7ce565efad960a
icham12345 <60590142+icham12345@users.noreply.github.com>
2020-02-03 14:17:21 +0700

    Add files via upload

--- index.blade.php
+++ index.blade.php
@@ -1,4 +1,4 @@
-@extends('table.layout')
+@extends('list.layout')
  
 @section('konten')
   @if(session()->get('success'))
@@ -6,45 +6,27 @@
       {{ session()->get('success') }}  
     </div><br/>
   @endif
-  <h1>Selamat Datang Tabel Hak Akses </h1>
-  <br>
- <div class="row justify-content-center">
-<div class="card shadow mb-4">
-            <div class="card-header py-3">
-              <h6 class="m-0 font-weight-bold text-primary">Table List Hak Akses</h6>
-            </div>
-            
-            <div class="card-body">
-              <div class="table-responsive">
-                <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
-                  <thead>
-                    <tr>
-                      <th>No</th>
-                      <th>Nama</th>
-                      <th>Hak Akses</th>
-                    </tr>
-                  </thead>
-                  <tfoot>
-                    <tr>
-                      <th>No</th>
-                      <th>Nama</th>
-                      <th>Hak Akses</th>
-                    </tr>
-                  </tfoot>
-                  <tbody>
-                     @foreach($tables as $table)
-                    <tr>
-                      <td>{{ $table->id }}</td>
-                      <td>{{ $table->nama }}</td>
-                      <td>{{ $table->hak_akses }}</td>
-                    </tr>
-                    @endforeach
-                  </tbody>
-                </table>
-                <a href="{{ route('forms.create')}}" class="btn btn-primary">Tambah</a>
+  <h1>Selamat Datang Di List API </h1>
+  <a href="{{ route('lists.create')}}" class="btn btn-primary">Tambah</a><br>
+  <div class="d-sm-flex align-items-center justify-content-between mb-4">
+            <h1 class="h3 mb-0 text-gray-800">LIST API</h1>
+          </div>
+ <div class="row">
+
+            <!-- Content Column -->
+            <div class="col-lg-6 mb-1">
+              <!-- Color System -->
+              <div class="row">
+                <div>
+                  @foreach($lists as $list)
+                  <div class="card bg-primary text-white shadow">
+                    <div class="card-body">
+                      <a class="text-black-50 large" href="{{ $list->url }}">{{ $list->nama }}</a>
+                    </div>
+                  </div>
+                   @endforeach
+                </div>
               </div>
             </div>
           </div>
-
-    </div>
 @endsection
